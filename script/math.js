9b8d34ded9a79344e5d1b03beb575e3d06cd3dcd
Laytonmar05 <marshalllt@g.crookcountyschools.org>
2021-02-23 11:50:35 -0800

    My Math Work

---
+++ script/math.js
@@ -0,0 +1,24 @@
+function add(a,b,c,d){
+    let sum = a + b + c + d
+    return sum;
+}
+
+function subtract(a,b){
+   let difference = a - b
+   return difference;
+}
+
+function multiply(a,b,c){
+    let product = a * b * c
+    return product;
+}
+
+function divide(a,b){
+    let quotient = a/b
+    return quotient;
+}
+a = 
+console.log(add(1,2,3,4))
+console.log(subtract(10,2))
+console.log(multiply(6,12,2))
+console.log(divide(10,5))
